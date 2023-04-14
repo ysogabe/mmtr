ca71f86381463051979aada05c43283bb1ebb665
Yoshio Sogabe <y.sogabe@gmail.com>
2023-04-14 14:41:22 +0900

    ADD: init

---
+++ Readme.md
@@ -0,0 +1,65 @@
+# BLE MAMORIO Scanner and Azure Event Hubs Sender
+
+本プロジェクトは、MAMORIOデバイスをBLE（Bluetooth Low Energy）でスキャンし、検出されたデータをAzure Event Hubsに送信するプログラムです。
+
+## 前提条件
+
+- Python 3.7 以上
+- [bluepy](https://github.com/IanHarvey/bluepy) ライブラリ
+- [azure-eventhub](https://pypi.org/project/azure-eventhub/) ライブラリ
+
+以下のコマンドで必要なパッケージをインストールできます。
+
+```bash
+pip install bluepy azure-eventhub
+```
+
+## 設定
+
+設定ファイル`config.ini`に以下の内容を記入してください。
+
+```ini
+[azure_eventhubs]
+connection_str = <your_connection_string>
+eventhub_name = <your_eventhub_name>
+```
+
+Azure Event Hubsの接続文字列とEvent Hub名を`<your_connection_string>`および`<your_eventhub_name>`に置き換えてください。
+
+また、環境変数`AZURE_CONNECTION_STR`および`AZURE_EVENTHUB_NAME`に同じ値を設定することもできます。
+
+## 実行方法
+
+このスクリプトではBLE（Bluetooth Low Energy）デバイスをスキャンするために、`bluepy`ライブラリを使用しています。`bluepy`ライブラリはLinux環境で動作し、一部の機能は特権ユーザー（rootユーザー）として実行する必要があります。
+
+したがって、本プログラムを実行する際は、`sudo`コマンドを使って特権ユーザーとして実行することをお勧めします。以下のコマンドでプログラムを実行できます。
+
+```bash
+sudo python mmtr.py
+```
+
+
+
+
+プログラムはBLEデバイスをスキャンし、MAMORIOが検出されると、そのデータをAzure Event Hubsに送信します。
+
+## 注意事項
+
+本プログラムは、Linux環境で動作することを前提としています。WindowsやmacOSでは動作しない可能性があります。
+開発環境はRaspberry PiなどのLinuxベースのシステムを推奨します。
+
+ただし、`sudo`を使うとシステム内のすべてのリソースにアクセス可能となるため、必要以上にアクセス許可が与えられることを避けるため、最小限の権限で実行できるようにすることが望ましいです。そのため、`capabilities`を使用することで、必要な権限のみ付与することができます。以下の手順で`capabilities`を設定し、スクリプトを実行できます。
+
+1. 実行ファイルに`setcap`コマンドで必要な権限を付与します。
+   ```bash
+   sudo setcap 'cap_net_raw,cap_net_admin+eip' /usr/bin/python3
+   ```
+
+   このコマンドはPython 3の実行ファイルに対して、必要な権限（`cap_net_raw`および`cap_net_admin`）を付与します。
+
+2. スクリプトを通常のユーザー（`sudo`なし）で実行します。
+   ```bash
+   python main.py
+   ```
+
+注意: `setcap`コマンドによる権限付与はセキュリティ上のリスクがあるため、十分に注意して実施してください。信頼できるプログラムであることを確認した上で実行し、状況に応じてリスクを評価してください。
---
+++ mmtr.py
@@ -0,0 +1,169 @@
+import sys
+import threading
+import os
+import configparser
+import signal
+import socket
+
+from time import sleep
+from datetime import datetime
+
+from bluepy.btle import Scanner, DefaultDelegate
+from azure.eventhub import EventHubProducerClient, EventData
+
+# 設定ファイルを読み込む
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Azure Event Hubs 接続情報
+CONNECTION_STR = os.environ.get(
+    "AZURE_CONNECTION_STR", config.get("azure_eventhubs", "connection_str")
+)
+EVENTHUB_NAME = os.environ.get(
+    "AZURE_EVENTHUB_NAME", config.get("azure_eventhubs", "eventhub_name")
+)
+
+# MAMORIOのUUID
+MAMORIO_UUID = "b9407f30f5f8466eaff925556b57fe6e"
+IBEACON_PREFIX = "4c000215"
+
+
+class ScanDelegate(DefaultDelegate):
+    """
+    BLEデバイスの検出イベントを処理するためにBluePyのDefaultDelegateクラスを継承したクラスです。
+    """
+
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def ibeacon_tx_power_to_dbm(self, hex_value: str) -> int:
+        """
+        このメソッドは、iBeaconのトランスミット電力値（hex_value）をデシベルミリワット（dBm）に変換します。
+
+        :param hex_value: 16進数形式のiBeaconトランスミット電力値
+        :type hex_value: str
+        :return: dBm 単位のトランスミット電力
+        :rtype: int
+        """
+
+        # 16進数の値を10進数に変換
+        decimal_value = int(hex_value, 16)
+
+        # 10進数が128以上の場合、256を引いて負の値に変換
+        if decimal_value >= 128:
+            tx_power_dbm = decimal_value - 256
+        else:
+            # それ以外の場合は、そのままの値を利用
+            tx_power_dbm = decimal_value
+
+        return tx_power_dbm
+
+    def hex_to_little_endian_decimal(self, hex_value: str) -> int:
+        """
+        リトルエンディアンの16進数値を10進数に変換します。
+
+        :param hex_value: str, 16進数値
+        :return: int, 与えられた16進数値から変換された10進数値
+        """
+
+        # Reverse byte order for little endian
+        little_endian_hex = ''.join(reversed([hex_value[i:i + 2] for i in range(0, len(hex_value), 2)]))
+
+        # Convert hexadecimal to decimal
+        decimal_value = int(little_endian_hex, 16)
+
+        return decimal_value
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        """
+        BLEデバイスが検出された際に呼び出されるメソッドです。
+        iBeaconのデータを処理し、MAMORIOが検出された場合はイベントハブにデータを送信します。
+
+        :param dev: 発見されたデバイスに関する詳細を含むデバイスオブジェクト
+        :param isNewDev: True の場合、新規検出デバイスです
+        :param isNewData: True の場合、デバイスから新しいデータを受信しました
+        """
+
+        try:
+            current_time = datetime.now()
+            if isNewDev or isNewData:
+                for (adtype, desc, value) in dev.getScanData():
+                    if adtype == 255 and value.startswith(IBEACON_PREFIX):
+                        uuid = value[8:40]
+                        major = self.hex_to_little_endian_decimal(value[40:44])
+                        minor = self.hex_to_little_endian_decimal(value[44:48])
+                        tx = value[48:50]
+
+                        if uuid.lower() == MAMORIO_UUID.lower():
+                            sender_thread = threading.Thread(target=send_to_eventhubs, args=(dev.addr, dev.rssi), daemon=True)
+                            sender_thread.start()
+        except Exception as e:
+            print(f"Error occurred in handleDiscovery: {e}")
+
+            
+def send_to_eventhubs(beacon_addr, rssi):
+    """
+    検出されたiBeaconのデータをAzure Event Hubsに送信します。追加のデータとして、送信時刻と送信ホスト名も含まれます。
+
+    :param beacon_addr: str, 検出されたビーコンのアドレス
+    :param rssi: int, 検出されたビーコンの信号強度（RSSI）
+    """
+
+    try:
+        producer = EventHubProducerClient.from_connection_string(CONNECTION_STR, EVENTHUB_NAME)
+
+        # 現在の時刻とホスト名を取得
+        timestamp = datetime.utcnow().isoformat()
+        host_name = socket.gethostname()
+
+        event_data = EventData(f"MAMORIO:{beacon_addr}, RSSI:{rssi}, Time:{timestamp}, Hostname:{host_name}")
+        producer.send_batch([event_data])
+    except Exception as e:
+        print(f"Error occurred during sending to Event Hubs: {e}")
+    finally:
+        if 'producer' in locals():
+            producer.close()
+
+            
+def signal_handler(sig, frame):
+    """
+    シグナルハンドラ: この関数は、特定のシグナルを受信したときに実行されます。
+
+    引数:
+        sig: 受信したシグナルの型（信号番号）
+        frame: 現在実行中のスタックフレーム
+
+    処理内容:
+        1. "終了処理中..."というメッセージを表示します。
+        2. プログラムを終了し、終了ステータス0でシステムに終了を通知します。
+    """
+    print("終了処理中...")
+    sys.exit(0)
+
+    
+# シグナルハンドラを登録
+signal.signal(signal.SIGINT, signal_handler)  # Ctrl+C用（キーボード割り込み）
+signal.signal(signal.SIGTERM, signal_handler)  # killコマンド用（終了要求）
+
+
+def start_scanning():
+    """
+    BLEデバイスの連続的なスキャンを開始します。
+    """
+
+    scanner = Scanner().withDelegate(ScanDelegate())
+    while True:
+        devices = scanner.scan(1.0)
+
+        
+if __name__ == "__main__":
+    # スキャニング用のスレッドを作成して実行
+    scan_thread = threading.Thread(target=start_scanning, daemon=True)
+    scan_thread.start()
+
+    # 無限ループ中にシグナルを待つ
+    try:
+        while True:
+            sleep(1)
+    except (KeyboardInterrupt, SystemExit):  # キーボード割り込みやシステム終了で終了処理
+        sys.exit()
